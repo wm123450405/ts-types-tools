83083d1c825f9c50689ef3202d1044cc99ad54ad
wangmiao <491029934@qq.com>
2025-08-28 16:51:13 +0800

    排序

--- src/array/core.ts
+++ src/array/core.ts
@@ -118,7 +118,7 @@ export type GenerateArray<T, L extends number> =
 		boolean extends Ti ? Li extends Li ? SimpleGenerateArray<Ti, Li> : never :
         Ti extends Ti ? Li extends Li ? SimpleGenerateArray<Ti, Li> : never : never : never;
 
-export type SimpleSortArray<T extends number[], L extends number[] = [], M extends number = Min<T>> =
+type SimpleSortArray<T extends number[], L extends number[] = [], M extends number = Min<T>> =
 	T extends [M, ...infer R extends number[]] ? [M, ...SimpleSortArray<[...L, ...R], []>] : 
 	T extends [infer F extends number, ...infer R extends number[]] ? SimpleSortArray<R, [...L, F], M> :
 	[];
